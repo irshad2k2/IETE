8e13bc7c857f4e294642fb1432df5b672fdf59cc
Irshad <mhdirshad2k2@gmail.com>
2023-09-03 16:05:12 +0530

    Add responsiveness

--- index.js
+++ index.js
@@ -208,32 +208,6 @@ window.onload = function () {
   
   new NavigationPage();
 
-// subscribe start
-function forceLower(strInput) 
-{
-strInput.value=strInput.value.toLowerCase();
-}
-
-Splitting();
-
-$('.signup').submit(function(event){
- event.preventDefault();
- 
- var emailText = $('.email').val();
- $(".field").append("<p data-splitting='chars'>" + emailText + "</p>");
-  Splitting();
-  $("#wrap").addClass("flip");
-  setTimeout(function(){	
-    $(".char, .word").addClass("slide");
-}, 1);
-  setTimeout(function(){	
-    $('.email').val('');
-    $( ".field p" ).remove();
-    $("#wrap").removeClass("flip");
-}, 3000);
-});
-// Subscribe end
-
 // Article start
 
 // Article end
